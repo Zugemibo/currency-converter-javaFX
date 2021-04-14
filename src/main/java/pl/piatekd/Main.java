a258ad12d8ad638bc4fe3877c5271095068677c1
Dawid <dawid.piatek1995@gmail.com>
2021-04-14 08:54:49 +0200

    Initial commit

---
+++ src/main/java/pl/piatekd/Main.java
@@ -0,0 +1,50 @@
+package pl.piatekd;
+	
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Main extends Application {
+	
+	private double xOffset = 0;
+    private double yOffset = 0;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("convert.fxml"));
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Currency Converter");
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+    
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/main/java/pl/piatekd/NewMain.java
@@ -0,0 +1,9 @@
+package pl.piatekd;
+
+public class NewMain {
+
+	public static void main(String[] args) {
+		Main.main(args);
+	}
+
+}
